7cda8d7ca577c4412be89d54c0fcdc017109ba77
LuckyHomie <68870079+LuckyHomie@users.noreply.github.com>
2022-10-06 17:36:29 +0200

    Add files via upload

---
+++ lab1/canvas.js
@@ -0,0 +1,32 @@
+function main(bckColor, circleColor) {
+    var canvas = document.getElementsByTagName("CANVAS")[0],
+      ctx = canvas.getContext("2d");
+    if (canvas.height) {
+      canvas.height = 800;
+    }
+    canvas.width = 600;
+    canvas.height = document.documentElement.scrollHeight;
+    var cW = canvas.width,
+      cH = canvas.height;
+    
+    ctx.fillStyle = bckColor || "#000";
+    ctx.fillRect(0, 0, cW, cH);
+    ctx.fill();
+  
+    ctx.beginPath();
+    ctx.fillStyle = circleColor || "#FFF";
+    ctx.shadowBlur = 5;
+    ctx.shadowColor = "#FFF";
+    ctx.arc(cW / 2, cH / 2, 50, 0, 2 * Math.PI, false);
+    ctx.fill();
+    ctx.closePath();
+  }
+  var colorArray = ['red', 'green', 'yellow', 'blue', 'pink', 'brown', 'orange']
+  var changeColor = setInterval(function() {
+  
+    let bckColor = colorArray[Math.floor(Math.random() * 7 + 1)];
+    let circleColor = colorArray[Math.floor(Math.random() * 7 + 1)];
+    main(bckColor, circleColor)
+  
+  }, 2000)
+  window.addEventListener("load", main);
