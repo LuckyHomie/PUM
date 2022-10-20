36a3034e32bd2cc2f3b1d6f5871558023336e92f
LuckyHomie <68870079+LuckyHomie@users.noreply.github.com>
2022-10-20 17:24:17 +0200

    Add files via upload

--- lab2/canvas.js
+++ lab2/canvas.js
@@ -5,19 +5,18 @@ canvas.height = 600;
 
 var c = canvas.getContext('2d');
 
-setTimeout(c.fillRect(0,100,100,500), 10000);
-setTimeout(c.fillRect(100,200,100,400), 10000);
-setTimeout(c.fillRect(200,300,100,300), 10000);
-setTimeout(c.fillRect(300,400,100,200), 10000);
-setTimeout(c.fillRect(400,500,100,100), 10000);
-setTimeout(function() {
-    var interval = setInterval(function() {
-        amount += 0.01; // change to alter duration
-        if (amount > 1) {
-            amount = 1;
-            clearInterval(interval);
-        }
-        c.fillRect(500,100,100,100);
-    }, 0);
-
-}, 3000);
+setTimeout(function () {
+        c.fillRect(0,100,100,500);
+    }, 1000);
+setTimeout(function () {
+        c.fillRect(100,200,100,400);
+    }, 2000);
+setTimeout(function () {
+        c.fillRect(200,300,100,300);
+    }, 3000);
+setTimeout(function () {
+        c.fillRect(300,400,100,200);
+    }, 4000);
+setTimeout(function () {
+    c.fillRect(400,500,100,100);
+}, 5000);
