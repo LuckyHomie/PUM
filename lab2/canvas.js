43a399c2e1fd96503e1a826ed48c27cd3a7e38e0
LuckyHomie <68870079+LuckyHomie@users.noreply.github.com>
2022-10-20 17:05:43 +0200

    Add files via upload

--- lab2/canvas.js
+++ lab2/canvas.js
@@ -4,3 +4,9 @@ canvas.width = 800;
 canvas.height = 600;
 
 var c = canvas.getContext('2d');
+
+c.fillRect(0,100,100,500);
+c.fillRect(100,200,100,400);
+c.fillRect(200,300,100,300);
+c.fillRect(300,400,100,200);
+c.fillRect(400,500,100,100);
