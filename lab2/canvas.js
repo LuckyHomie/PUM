78941b242604b223ad28e99adae422ca6e552957
LuckyHomie <68870079+LuckyHomie@users.noreply.github.com>
2022-10-20 17:51:30 +0200

    Add files via upload

--- lab2/canvas.js
+++ lab2/canvas.js
@@ -3,6 +3,26 @@ var canvas = document.querySelector('canvas');
 canvas.width = 800;
 canvas.height = 600;
 
+function Ball(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+
+    this.update = function() {
+        this.y += 1;
+        this.draw();
+    };
+
+    this.draw = function() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill();
+        c.closePath();
+    }
+}
+
 var c = canvas.getContext('2d');
 
 setTimeout(function () {
@@ -20,3 +40,11 @@ setTimeout(function () {
 setTimeout(function () {
     c.fillRect(700,100,100,100);
     }, 5000);
+var ball;
+setTimeout(function () {
+    c.beginPath();
+        c.arc(750, 50, 50, 0, Math.PI * 2, false);
+        c.fillStyle = 'red';
+        c.fill();
+        c.closePath();
+    }, 6000);
